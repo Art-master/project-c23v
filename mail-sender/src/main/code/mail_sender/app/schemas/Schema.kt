3fdb677df986bfd75db1d49593ae615510540c1d
Art-_-master <ggn.master@gmail.com>
2021-11-04 23:25:24 +0400

    [REFACTOR] move mail-sender service in own repository

--- mail-sender/src/main/code/mail_sender/app/schemas/Schema.kt
+++
@@ -1,9 +0,0 @@
-package code.mail_sender.app.schemas
-
-object Schema {
-    const val TABLE_NAME = "mail"
-    const val ID = "id"
-    const val ADDRESS = "address"
-    const val SUBJECT = "subject"
-    const val TEXT = "text"
-}
