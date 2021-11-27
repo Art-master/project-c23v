36dd100b51dd75cbc33f0842fe36443287ea98a6
Art-_-master <ggn.master@gmail.com>
2021-11-27 23:03:14 +0400

    [FEATURE] improve AuthenticationController

--- core/src/main/com/core/app/controllers/AuthenticationController.kt
+++ core/src/main/com/core/app/controllers/AuthenticationController.kt
@@ -1,24 +1,32 @@
 package com.core.app.controllers
 
 import com.core.app.services.AuthenticationService
-import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
 
 
 @RestController
 @RequestMapping("/auth")
-class AuthenticationController(
-    val messageSource: ResourceBundleMessageSource,
-    val authenticationService: AuthenticationService
-) {
+class AuthenticationController(val authenticationService: AuthenticationService) {
 
     @GetMapping("/send_sms/{phone_number}")
     fun sendSms(@PathVariable("phone_number") phoneNumber: String) {
         authenticationService.sendSms()
     }
 
-    @GetMapping("/waiting_for_call/{phone_number}")
-    fun waitingForCall(@PathVariable("phone_number") phoneNumber: String) {
-        authenticationService.waitingForCall()
+    @PostMapping("confirm_phone_number")
+    fun confirmPhoneNumber(
+        @RequestParam(required = false, defaultValue = "ru_RU") lang: String = "ru_RU",
+        @RequestParam("phone_number") phone: String
+    ) {
+        return authenticationService.confirmPhoneNumber(phone)
+    }
+
+    @GetMapping("is_phone_number_confirmed")
+    fun isPhoneNumberConfirmed(
+        @RequestParam(required = false, defaultValue = "ru_RU") lang: String,
+        @RequestParam("phone_number") phone: String
+    ): Mono<Boolean> {
+        return authenticationService.isPhoneNumberConfirmed(phone)
     }
 }
--- core/src/main/com/core/app/services/AuthenticationService.kt
+++ core/src/main/com/core/app/services/AuthenticationService.kt
@@ -1,16 +1,36 @@
 package com.core.app.services
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Service
-class AuthenticationService {
+class AuthenticationService(val messageSource: ResourceBundleMessageSource) {
+
+    @Value("\${TELECOM_SERVICE_ADDR}")
+    private lateinit var telecomServiceAddr: String
+
+    private val webClient = WebClient.create()
 
     fun sendSms() {
 
     }
 
-    fun waitingForCall() {
+    fun confirmPhoneNumber(phoneNumber: String) {
+        webClient.get()
+            .uri("$telecomServiceAddr/confirm_phone_number")
+            .attribute("phone_number", phoneNumber)
+            .retrieve()
+    }
 
+    fun isPhoneNumberConfirmed(phoneNumber: String): Mono<Boolean> {
+        return webClient.get()
+            .uri("$telecomServiceAddr/is_phone_number_confirmed")
+            .attribute("phone_number", phoneNumber)
+            .retrieve()
+            .bodyToMono(Boolean::class.java)
     }
 
 }
